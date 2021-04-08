6f6093a144844565ed86de1d8194be78aa3734b6
Sibing <1915563868@qq.com>
2021-04-08 21:27:01 +0800

    Set up maven project

---
+++ src/test/java/cz3002/73/AppTest.java
@@ -0,0 +1,20 @@
+package cz3002.73;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
